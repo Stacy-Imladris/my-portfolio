4774e94fc9c791acad0b499a39506f9feaaa2559
stacy <ada.davis27@gmail.com>
2022-05-05 23:45:19 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import {Header} from "./components/header/Header";
-import {Main} from './components/main/Main';
-import {Skills} from "./components/skills/Skills";
-import {Projects} from "./components/projects/Projects";
-import {Freelance} from "./components/freelance/Freelance";
-import {Contacts} from "./components/contacts/Contacts";
-import {Footer} from "./components/footer/Footer";
+import {Contacts, Footer, Freelance, Header, Main, Projects, Skills} from './components';
 import s from './App.module.scss';
 
 export const App = () => {
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,17 @@
+import {Contacts} from './contacts/Contacts'
 import {Footer} from './footer/Footer'
+import {Freelance} from './freelance/Freelance'
+import {Header} from './header/Header'
+import {Main} from './main/Main'
+import {Projects} from './projects/Projects'
+import {Skills} from './skills/Skills'
 
 export {
-    Footer
+    Footer,
+    Contacts,
+    Freelance,
+    Header,
+    Main,
+    Projects,
+    Skills
 }
