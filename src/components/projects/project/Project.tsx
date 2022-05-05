44fcbce2fe7d5f1387e117dca6b8d73434801312
stacy <ada.davis27@gmail.com>
2022-05-05 23:36:39 +0300

    changes

--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -1,7 +1,6 @@
 import s from './Project.module.scss';
 import {Fade} from 'react-awesome-reveal';
 import {Button} from '../../../common/components/button/Button';
-import {useState} from 'react';
 
 type ProjectPropsType = {
     title: string
@@ -10,28 +9,18 @@ type ProjectPropsType = {
     projectUrl: string
 }
 
-export const Project = ({title, description, imgUrl, projectUrl}: ProjectPropsType) => {
-    const [isBtnShown, setIsBtnShown] = useState<boolean>(false)
-
-    const showButton = () => {
-        setIsBtnShown(true)
-    }
-
-    const hideButton = () => {
-        setIsBtnShown(false)
-    }
-
-    return (
-        <div className={s.project}>
-            <Fade>
-                <div className={s.image} style={{backgroundImage: imgUrl}} onMouseEnter={showButton} onMouseLeave={hideButton}>
-                    {isBtnShown && <Button text={'Open'} url={projectUrl}/>}
+export const Project = ({title, description, imgUrl, projectUrl}: ProjectPropsType) => (
+    <div className={s.project}>
+        <Fade>
+            <div className={s.image} style={{backgroundImage: imgUrl}}>
+                <div className={s.button}>
+                    <Button text={'Open'} url={projectUrl}/>
                 </div>
-                <div className={s.projectInfo}>
-                    <h3 className={s.title}>{title}</h3>
-                    <span className={s.description}>{description}</span>
-                </div>
-            </Fade>
-        </div>
-    )
-}
+            </div>
+            <div className={s.projectInfo}>
+                <h3 className={s.title}>{title}</h3>
+                <span className={s.description}>{description}</span>
+            </div>
+        </Fade>
+    </div>
+)
