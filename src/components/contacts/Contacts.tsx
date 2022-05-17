533d2443b12dcbc29df6cf5c9ddce06527d02e31
stacy <ada.davis27@gmail.com>
2022-05-17 13:41:50 +0300

    node js form added

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -2,26 +2,28 @@ import s from './Contacts.module.scss';
 import {Title} from '../../common/components/title/Title';
 import {Fade} from 'react-awesome-reveal';
 import axios from 'axios';
-import {useRef} from 'react';
+import {FormEvent, useRef} from 'react';
 
 export const Contacts = () => {
     const nameRef = useRef<HTMLInputElement>(null);
     const emailRef = useRef<HTMLInputElement>(null);
     const messageRef = useRef<HTMLTextAreaElement>(null);
 
+    const onSubmitSendMessage = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        axios.post('https://form-for-portfolio.herokuapp.com/sendMessage', {
+            name: nameRef.current?.value,
+            email: emailRef.current?.value,
+            message: messageRef.current?.value
+        })
+    }
+
     return (
         <div className={s.mainBlock} id={'contacts'}>
             <Fade delay={150}>
                 <div className={s.contactContainer}>
                     <Title text={'Contact'}/>
-                    <form className={s.form} onSubmit={(e) => {
-                        e.preventDefault()
-                        axios.post('https://form-for-portfolio.herokuapp.com/sendMessage', {
-                            name: nameRef.current?.value,
-                            email: emailRef.current?.value,
-                            message: messageRef.current?.value
-                        })
-                    }}>
+                    <form className={s.form} onSubmit={onSubmitSendMessage}>
                         <input placeholder={'Name'} className={s.input} ref={nameRef}/>
                         <input placeholder={'E-mail'} className={s.input} ref={emailRef}/>
                         <textarea placeholder={'Your message'} className={s.area} ref={messageRef}/>
