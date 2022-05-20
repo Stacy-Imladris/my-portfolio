d29c3bc02e71670cfd1399416e8362a8d6c011b9
stacy <ada.davis27@gmail.com>
2022-05-20 22:49:49 +0300

    changes

--- src/components/freelance/Freelance.tsx
+++ src/components/freelance/Freelance.tsx
@@ -1,13 +1,17 @@
 import s from './Freelance.module.scss';
 import {Fade} from 'react-awesome-reveal';
 import {Title} from '../../common/components/title/Title';
+import {Link} from 'react-scroll';
 
 export const Freelance = () => (
     <div className={s.mainBlock}>
         <Fade delay={150}>
             <div className={s.freelanceContainer}>
                 <Title text={'I Am Available For Freelancer'}/>
-                <button className={s.button}>HIRE ME</button>
+                <Link className={s.button} to={'contacts'} spy={true} smooth={true}
+                      offset={-70} duration={300}>
+                    HIRE ME
+                </Link>
             </div>
         </Fade>
     </div>
