65f199da3c59e0272348110eb3e084dff587de28
stacy <ada.davis27@gmail.com>
2023-09-16 15:38:21 +0300

    refactoring

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,10 @@
 import s from './Header.module.scss';
-import {Nav} from "./nav/Nav";
+import {Nav} from './nav/Nav';
 import {BurgerNav} from './burgerNav/BurgerNav';
 
-export const Header = () => {
-    return (
-        <div className={s.header}>
-            <Nav/>
-            <BurgerNav/>
-        </div>
-    )
-}
+export const Header = () => (
+    <div className={s.header}>
+        <Nav/>
+        <BurgerNav/>
+    </div>
+)
