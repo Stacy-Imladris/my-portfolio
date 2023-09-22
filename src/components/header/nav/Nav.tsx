a99c3686ba2fd6628c406d3dd1e9c7ae092acaa1
stacy <ada.davis27@gmail.com>
2023-09-23 00:22:57 +0300

    refactoring

--- src/components/header/nav/Nav.tsx
+++ src/components/header/nav/Nav.tsx
@@ -1,13 +1,11 @@
 import s from './Nav.module.scss';
 import {UniversalLink} from 'common/components/universalLink/UniversalLink';
 
-export const Nav = () => {
-    return (
-        <div className={s.nav}>
-            <UniversalLink anchor={'main'} offset={-70} text={'Main'}/>
-            <UniversalLink anchor={'skills'} offset={-50} text={'Skills'}/>
-            <UniversalLink anchor={'projects'} offset={-50} text={'Projects'}/>
-            <UniversalLink anchor={'contacts'} offset={-70} text={'Contacts'}/>
-        </div>
-    )
-}
+export const Nav = () => (
+    <div className={s.nav}>
+        <UniversalLink anchor={'main'} offset={-70} text={'Main'}/>
+        <UniversalLink anchor={'skills'} offset={-50} text={'Skills'}/>
+        <UniversalLink anchor={'projects'} offset={-50} text={'Projects'}/>
+        <UniversalLink anchor={'contacts'} offset={-70} text={'Contacts'}/>
+    </div>
+)
