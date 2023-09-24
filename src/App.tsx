5cfd7b85ae53c85bf0ca8b9720fbfd4facdadd30
stacy <ada.davis27@gmail.com>
2023-09-24 12:04:02 +0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import {Contacts, Footer, Freelance, Header, Main, Projects, Skills} from './components';
 import s from './App.module.scss';
 
-export const App = () => {
-    return (
-        <div className={s.app}>
-            <Header/>
-            <Main/>
-            <Skills/>
-            <Projects/>
-            <Freelance/>
-            <Contacts/>
-            <Footer/>
-        </div>
-    )
-}
+export const App = () => (
+    <div className={s.app}>
+        <Header/>
+        <Main/>
+        <Skills/>
+        <Projects/>
+        <Freelance/>
+        <Contacts/>
+        <Footer/>
+    </div>
+)
