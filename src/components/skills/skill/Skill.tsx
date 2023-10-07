38df358d388a2d4084890d1f3a90f7421e205808
stacy <ada.davis27@gmail.com>
2023-10-07 20:59:15 +0300

    refactoring

--- src/components/skills/skill/Skill.tsx
+++ src/components/skills/skill/Skill.tsx
@@ -1,12 +1,12 @@
 import s from './Skill.module.scss';
 import {Fade} from 'react-awesome-reveal';
 
-type SkillPropsType = {
+type Props = {
     title: string
     imgUrl: string
 }
 
-export const Skill = ({title, imgUrl}: SkillPropsType) => (
+export const Skill = ({title, imgUrl}: Props) => (
     <div className={s.skill}>
         <Fade>
             <div className={s.icon}>
