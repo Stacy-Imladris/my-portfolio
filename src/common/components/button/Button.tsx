2bdcd8adf16d4be920507d38a5531e3cbc192667
stacy <ada.davis27@gmail.com>
2023-10-14 19:15:40 +0300

    refactoring

--- src/common/components/button/Button.tsx
+++ src/common/components/button/Button.tsx
@@ -1,10 +1,10 @@
 import s from './Button.module.scss'
 
-type ButtonPropsType = {
+type Props = {
     text: string
     url: string
 }
 
-export const Button = ({text, url}: ButtonPropsType) => (
+export const Button = ({text, url}: Props) => (
     <a href={url} className={s.btn} target='_blank'>{text}</a>
 )
