6587cb1d6706dbe22fe2abc2243847e9c7283234
stacy <ada.davis27@gmail.com>
2023-10-30 19:12:56 +0300

    refactoring

--- src/common/components/universalLink/UniversalLink.tsx
+++ src/common/components/universalLink/UniversalLink.tsx
@@ -1,14 +1,14 @@
 import {Link} from 'react-scroll'
 import s from './UniversalLink.module.scss'
 
-type LinkPropsType = {
+type Props = {
     anchor: string
     offset: number
     text: string
     onClickHandle?: () => void
 }
 
-export const UniversalLink = ({anchor, offset, text, onClickHandle}: LinkPropsType) => (
+export const UniversalLink = ({anchor, offset, text, onClickHandle}: Props) => (
     <Link activeClass={s.active} to={anchor} spy={true} smooth={true} offset={offset}
           duration={300} onClick={onClickHandle}>
         {text}
