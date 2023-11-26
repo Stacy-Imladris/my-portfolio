d12f1c6afb71e37e6a92edf4d6a15353b3a881fb
stacy <ada.davis27@gmail.com>
2023-11-26 20:14:46 +0300

    refactoring

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
-import s from './Header.module.scss';
-import {Nav} from './nav/Nav';
-import {BurgerNav} from './burgerNav/BurgerNav';
+import s from './Header.module.scss'
+import {Nav} from './nav/Nav'
+import {BurgerNav} from './burgerNav/BurgerNav'
 
 export const Header = () => (
     <div className={s.header}>
