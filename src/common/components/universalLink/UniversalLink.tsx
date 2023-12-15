d58eb5bb955e0340a15392877d89c46784b39b1f
stacy <ada.davis27@gmail.com>
2023-12-15 18:12:30 +0300

    refactoring

--- src/common/components/universalLink/UniversalLink.tsx
+++ src/common/components/universalLink/UniversalLink.tsx
@@ -5,12 +5,12 @@ type Props = {
     anchor: string
     offset: number
     text: string
-    onClickHandle?: () => void
+    onClick?: () => void
 }
 
-export const UniversalLink = ({anchor, offset, text, onClickHandle}: Props) => (
+export const UniversalLink = ({anchor, offset, text, onClick}: Props) => (
     <Link activeClass={s.active} to={anchor} spy={true} smooth={true} offset={offset}
-          duration={300} onClick={onClickHandle}>
+          duration={300} onClick={onClick}>
         {text}
     </Link>
 )
--- src/components/header/burgerNav/BurgerNav.tsx
+++ src/components/header/burgerNav/BurgerNav.tsx
@@ -14,10 +14,10 @@ export const BurgerNav = () => {
     return (
         <div className={s.burgerNav}>
             <div className={burgerNavItemsStyle}>
-                <UniversalLink anchor={'main'} offset={-70} text={'Main'} onClickHandle={hideMenu}/>
-                <UniversalLink anchor={'skills'} offset={-50} text={'Skills'} onClickHandle={hideMenu}/>
-                <UniversalLink anchor={'projects'} offset={-50} text={'Projects'} onClickHandle={hideMenu}/>
-                <UniversalLink anchor={'contacts'} offset={-70} text={'Contacts'} onClickHandle={hideMenu}/>
+                <UniversalLink anchor={'main'} offset={-70} text={'Main'} onClick={hideMenu}/>
+                <UniversalLink anchor={'skills'} offset={-50} text={'Skills'} onClick={hideMenu}/>
+                <UniversalLink anchor={'projects'} offset={-50} text={'Projects'} onClick={hideMenu}/>
+                <UniversalLink anchor={'contacts'} offset={-70} text={'Contacts'} onClick={hideMenu}/>
             </div>
             <div className={s.burgerButton} onClick={openMenu}>
                 <div className={isOpenMenu ? s.open : ''}/>
