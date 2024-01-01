bcceeb4da03288e6c8957a40ffca01f09bd622f7
stacy <ada.davis27@gmail.com>
2024-01-01 14:04:38 +0300

    refactoring

--- src/components/header/nav/Nav.tsx
+++ src/components/header/nav/Nav.tsx
@@ -1,11 +1,15 @@
 import s from './Nav.module.scss'
 import {UniversalLink} from 'common/components/universalLink/UniversalLink'
 
-export const Nav = () => (
-    <div className={s.nav}>
-        <UniversalLink anchor={'main'} offset={-70} text={'Main'}/>
-        <UniversalLink anchor={'skills'} offset={-50} text={'Skills'}/>
-        <UniversalLink anchor={'projects'} offset={-50} text={'Projects'}/>
-        <UniversalLink anchor={'contacts'} offset={-70} text={'Contacts'}/>
-    </div>
-)
+const links = [
+    {anchor: 'main', offset: -70, text: 'Main'},
+    {anchor: 'skills', offset: -50, text: 'Skills'},
+    {anchor: 'projects', offset: -50, text: 'Projects'},
+    {anchor: 'contacts', offset: -70, text: 'Contacts'},
+]
+
+export const Nav = () => {
+    const linksList = links.map(link => <UniversalLink {...link} key={link.text}/>)
+
+    return <div className={s.nav}>{linksList}</div>
+}
