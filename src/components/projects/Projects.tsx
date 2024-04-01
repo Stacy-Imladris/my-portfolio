6acd0ea8803a750f2ea4908a31e70c058c329e2a
stacy <ada.davis27@gmail.com>
2024-04-01 22:03:41 +0300

    updated portfolio

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,26 +1,23 @@
 import s from './Projects.module.scss'
 import {Project} from './project/Project'
-import {Title} from 'common/components/title/Title'
-import todolist from 'assets/images/projects/todolist.jpg'
-import socialNetwork from 'assets/images/projects/socialNetwork.jpg'
-import brainstorm from 'assets/images/projects/brainstorm.jpg'
-import movies from 'assets/images/projects/movies.jpg'
+import itIncubator from 'assets/images/projects/itIncubator.webp'
+import todolist from 'assets/images/projects/todolist.webp'
+// import socialNetwork from 'assets/images/projects/socialNetwork.webp'
+import brainstorm from 'assets/images/projects/brainstorm.webp'
+import movies from 'assets/images/projects/movies.webp'
 import {Container} from 'components/Container/Container';
 
 const projects = [
-    {title: 'Brainstorm', githubName: 'cards', image: brainstorm},
-    {title: 'Todolist', githubName: 'todolist', image: todolist},
-    {title: 'Movies Catalog', githubName: 'movies', image: movies},
-    {title: 'Social Network', githubName: 'it-react', image: socialNetwork},
+    {title: 'IT-Incubator Website', url: 'https://it-incubator.io', image: itIncubator},
+    {title: 'Brainstorm', url: 'https://stacy-imladris.github.io/cards', image: brainstorm},
+    {title: 'Movies Catalog', url: 'https://stacy-imladris.github.io/movies', image: movies},
+    {title: 'Todolist', url: 'https://stacy-imladris.github.io/todolist', image: todolist},
+    // {title: 'Social Network', url: 'https://stacy-imladris.github.io/it-react', image: socialNetwork},
 ]
 
 export const Projects = () => {
-    const projectsList = projects.map(({title, githubName, image}) => (
-        <Project key={title}
-                 imgUrl={`url(${image})`}
-                 title={title}
-                 projectName={githubName}
-                 description={''}/>
+    const projectsList = projects.map(project => (
+        <Project key={project.title} {...project}/>
     ))
 
     return (
--- src/components/projects/project/Project.tsx
+++ src/components/projects/project/Project.tsx
@@ -1,26 +1,35 @@
+import {useState} from 'react';
 import s from './Project.module.scss'
 import {Fade} from 'react-awesome-reveal'
 import {Button} from 'common/components/button/Button'
 
-const baseGithubLink = 'https://stacy-imladris.github.io'
-
 type Props = {
-    title: string
-    description: string
-    imgUrl: string
-    projectName: string
+  title: string
+  image: string
+  url: string
+  description?: string
 }
 
-export const Project = ({title, description, imgUrl, projectName}: Props) => (
-    <div className={s.project}>
+export const Project = ({title, description, image, url}: Props) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false)
+
+  const makeHovered = () => setIsHovered(true)
+  const makeNotHovered = () => setIsHovered(false)
+
+  return (
+      <div className={s.project}>
         <Fade>
-            <div className={s.image} style={{backgroundImage: imgUrl}}>
-                <Button text={'Open'} url={`${baseGithubLink}/${projectName}`}/>
-            </div>
-            <div className={s.projectInfo}>
-                <h3 className={s.title}>{title}</h3>
-                <span className={s.description}>{description}</span>
-            </div>
+          <div className={s.image} style={{backgroundImage: `url(${image})`}}
+               onMouseEnter={makeHovered}
+               onMouseDown={makeHovered}
+               onMouseLeave={makeNotHovered}>
+            <span hidden={!isHovered}><Button text={'Open'} url={url}/></span>
+          </div>
+          <div className={s.projectInfo}>
+            <h3 className={s.title}>{title}</h3>
+            <span className={s.description}>{description}</span>
+          </div>
         </Fade>
-    </div>
-)
+      </div>
+  )
+}
